8ff0669269eb6384f4aecd7e53fb81276857475d
shravanpm <97494889+shravanpm@users.noreply.github.com>
2022-04-03 10:39:01 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const connect = () =>{
-    return mongoose.connect(
-     "mongodb://127.0.0.1:27017/u4c4project"
-    ); 
- };
- module.exports = connect
--- src/src/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-
-const connect = async () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/u4c4project");
-  };
-
-  module.exports = connect
