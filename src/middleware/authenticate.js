4de2354c31215a60c01175b06adc00da0734580e
shravanpm <97494889+shravanpm@users.noreply.github.com>
2022-04-03 13:11:44 +0530

    Update authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+//require("dotenv").config();
 const jwt = require("jsonwebtoken")
 
 
 const verifyToken = (token) => {
     return new Promise((resolve,reject) => {
-        jwt.verify(token, process.env.SECRET_KEY, (err,decoded) => {
+        jwt.verify(token, "masai", (err,decoded) => {
             if(err) return reject(err)
     
             return resolve(decoded)
